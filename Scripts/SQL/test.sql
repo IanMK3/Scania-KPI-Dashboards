effa03ae4cde6df9b52c066e07ee9c540b64238c
vamsi bora <vamsi.bora@oneadvanced.com>
2018-11-14 10:44:11 +0000

    test sql

---
+++ Scripts/SQL/test.sql
@@ -0,0 +1,11 @@
+select 
+--*  ,
+sum(w.value)
+from 
+fact.wastecost w
+inner join dim.date d on d.Date=w.date
+inner join dim.branch b on b.id=w.Branch
+where d.month=11 and d.year=2018
+and b.[Branch Code]='5435'
+
+--and w.[SL PL Acc Code]='W56909'
