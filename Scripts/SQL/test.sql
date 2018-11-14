e28685d767a27ce18c49d2e5fbe2cae25754008c
Acheampong Eric <eric.acheampong@scania.com>
2018-11-14 10:52:00 +0000

    testing merge

--- Scripts/SQL/test.sql
+++ Scripts/SQL/test.sql
@@ -8,8 +8,5 @@ inner join dim.branch b on b.id=w.Branch
 where d.month=11 and d.year=2018
 and b.[Branch Code]='5435'
 
-<<<<<<< HEAD
---eric
-=======
+--eric eric
 --and w.[SL PL Acc Code]='W56909' --vamsi
->>>>>>> bd19b7a536ca127ed83b44d921b92704ad03b0df
