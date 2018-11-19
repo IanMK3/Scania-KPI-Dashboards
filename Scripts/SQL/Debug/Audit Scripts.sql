bd92893dd498fce74135584213ff82a948ed9005
EAC010 <EAC010@GBQQBDBL01>
2018-11-19 17:58:33 +0000

    updates

--- Scripts/SQL/Debug/Audit Scripts.sql
+++ Scripts/SQL/Debug/Audit Scripts.sql
@@ -11,12 +11,12 @@ select * from audit.batch order by id  desc   /*  contains the complete batch*/
 
 
 
-select * from audit.package where batch = 131  /* contains details of ssis packages in a batch*/
+select * from audit.package where batch = 149  /* contains details of ssis packages in a batch*/
 
 
 
 
-select * from audit.Task where package = 456  /* Detail individual task in the package*/
+select * from audit.Task where package = 521  /* Detail individual task in the package*/
 
 
 
